a18f6363697c058c31164bbfb32cbf7d7d4c2bfa
阿川 <pgc2018@126.com>
2021-06-27 23:19:26 +0800

    init commit

---
+++ src/components/forumHome/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="aaa">
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>
+          <strong>此项目</strong> by <a href="#">彭广川</a>、<a href="#"
+            >叶锡烨</a
+          >。
+        </p>
+        <p><a href="">前端项目开源地址</a> <a href="">后端项目开源地址</a></p>
+      </div>
+    </footer>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created() {},
+};
+</script>
+
+<style scoped>
+</style>
+
---
+++ src/components/forumHome/Top.vue
@@ -0,0 +1,177 @@
+<template>
+  <nav class="navbar is-light" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="#">
+        <img src="../../../public/fav.png" />
+      </a>
+      <a
+        role="button"
+        class="navbar-burger burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item" @click="goHome">首页</a>
+        <a class="navbar-item" @click="goRead">读书</a>
+        <a class="navbar-item" @click="goListen">音乐</a>
+        <a class="navbar-item" @click="goWatch">电影</a>
+        <!-- <a class="navbar-item">热榜</a> -->
+        <!-- <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">更多</a>
+          <div class="navbar-dropdown">
+            <a class="navbar-item" @click="userhome">个人主页</a>
+            <a class="navbar-item">全部板块</a>
+            <a class="navbar-item">全部帖子</a>
+            <hr class="navbar-divider" />
+            <a class="navbar-item">Report an issue</a>
+          </div>
+        </div> -->
+        <div class="level-left">
+          <div class="level-item">
+            <p class="subtitle is-5"></p>
+          </div>
+          <div class="level-item">
+            <div class="field has-addons">
+              <p class="control">
+                <input class="input" type="text" placeholder="Find a post" />
+              </p>
+              <p class="control">
+                <button class="button">搜索</button>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="navbar-end" v-if="!$store.state.isLogin">
+        <div class="navbar-item">
+          <div class="buttons">
+            <b-button type="is-info" outlined @click="register">注册</b-button>
+            <div class="navbar-menu">
+              <div class="navbar-end">
+                <b-dropdown
+                  position="is-bottom-left"
+                  aria-role="menu"
+                  trap-focus
+                >
+                  <a class="navbar-item" slot="trigger" role="button">
+                    <b-button type="is-info" outlined>登录</b-button>
+                  </a>
+
+                  <b-dropdown-item
+                    aria-role="menu-item"
+                    :focusable="false"
+                    custom
+                    paddingless
+                  >
+                    <div class="modal-card" style="width: 300px">
+                      <section class="modal-card-body">
+                        <b-field label="Email">
+                          <b-input
+                            type="email"
+                            v-model="email"
+                            placeholder="Your email"
+                            required
+                          ></b-input>
+                        </b-field>
+
+                        <b-field label="密码">
+                          <b-input
+                            type="password"
+                            password-reveal
+                            placeholder="Your password"
+                            required
+                            v-model="password"
+                          ></b-input>
+                        </b-field>
+
+                        <b-checkbox>记住我</b-checkbox>
+                      </section>
+                      <footer class="modal-card-foot">
+                        <button class="button is-primary" @click="login">
+                          登录
+                        </button>
+                      </footer>
+                    </div>
+                  </b-dropdown-item>
+                </b-dropdown>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div v-else-if="$store.state.isLogin">
+        <div>
+          <i class="far fa-address-card">{{ $store.state.user.userName }}</i>
+        </div>
+        <div @click="logout">
+          <i class="fas fa-sign-out-alt">登出</i>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { userLogin } from "@/api";
+
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    goHome() {
+      this.$router.go(0);
+    },
+    goRead(){
+      console.log('read');
+    },
+    goListen(){
+      console.log('listen');
+    },
+    goWatch(){
+      console.log('Watch');
+    },
+    logout() {
+      this.$store.commit("logout");
+    },
+    userhome() {
+      this.$router.push("/userhome");
+    },
+    login() {
+      userLogin(this.email, this.password)
+        .then((res) => {
+          const { data } = res;
+          this.user = data;
+          console.log(data);
+          if (data != null) {
+            this.$store.dispatch("aLogin", {
+              user: data,
+              message: "牛逼",
+              success: () => {
+                console.log("欢迎您");
+              },
+            });
+          } else {
+            alert("该用户不存在");
+          }
+        })
+        .catch(() => {});
+    },
+    register() {
+      this.$router.push("/registerhome");
+    },
+  },
+};
+</script>
